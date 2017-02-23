0149a6bc54e1f69b45b78bbb6a791ce633a82519
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-02-23 11:54:14 +1100

    (#8) fixed tests
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- tests/tester.js
+++ tests/tester.js
@@ -30,6 +30,27 @@ const Fs = require(`fs`);
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 const TESTER = (() => { //constructor factory
 
+	/**
+	 * PRIVATE
+	 * Flatten a deep object into a one level object with it’s path as key
+	 *
+	 * @param  {object} object - The object to be flattened
+	 *
+	 * @return {object}        - The resulting flat object
+	 */
+	const flatten = object => {
+		return Object.assign( {}, ...function _flatten( objectBit, path = '' ) {  //spread the result into our return object
+			return [].concat(                                                       //concat everything into one level
+				...Object.keys( objectBit ).map(                                      //iterate over object
+					key => typeof objectBit[ key ] === 'object' ?                       //check if there is a nested object
+						_flatten( objectBit[ key ], `${ path }/${ key }` ) :              //call itself if there is
+						( { [ `${ path }/${ key }` ]: objectBit[ key ] } )                //append object with it’s path as key
+				)
+			)
+		}( object ) );
+	};
+
+
 	return {
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Settings
@@ -44,7 +65,6 @@ const TESTER = (() => { //constructor factory
 					options: [],
 				},
 				compare: 'pancake/',
-				hash: 'c7339efc1cda7858d7b6d74f10627010486429071ef04b4143962ac2aa94bc24',
 			},
 			{
 				name: 'Batter test with five modules and modules enabled',
@@ -54,7 +74,6 @@ const TESTER = (() => { //constructor factory
 					options: [],
 				},
 				compare: 'pancake/',
-				hash: '0c9ecfaf56c48faae00e32fc845c61b8113582ca618eabc280a0edf61aea8089',
 			},
 			{
 				name: 'Batter test with orgName overwrite and minification off',
@@ -64,7 +83,6 @@ const TESTER = (() => { //constructor factory
 					options: [ '--org', '@other.org' ],
 				},
 				compare: 'pancake/',
-				hash: '66b74390f6ad8366e007806f9b30fb9e3a04285f92027eef38435d48dca67cd3',
 			},
 			{
 				name: 'Syrup test with folder overwrite',
@@ -74,7 +92,6 @@ const TESTER = (() => { //constructor factory
 					options: [],
 				},
 				compare: 'testfolder/',
-				hash: 'f89f36ff87419101ce16163ddb82792138c1e2d2cb191bd700aaa35579c4f53d',
 			},
 		],
 
@@ -93,31 +110,34 @@ const TESTER = (() => { //constructor factory
 
 				allTasks.push(
 					TESTER
-						.delete( scriptFolder, unit )                       //delete trash first
-						.then( () => TESTER.run( scriptFolder, unit ) )     //now run script
-						.then( () => TESTER.compare( scriptFolder, unit ) ) //now compare output
-						.catch( error => TESTER.log.error( error ) )        //catch errors...
+						.delete( scriptFolder, unit )                                   //delete trash first
+						.then( () => TESTER.run( scriptFolder, unit ) )                 //now run script
+						.then( () => TESTER.fixture( scriptFolder, unit ) )             //get hash for fixture
+						.then( result => TESTER.result( scriptFolder, unit, result ) )  //get hash for result of test
+						.then( result => TESTER.compare( unit, result ) )               //now compare both and detail errors
+						.then( () => TESTER.delete( scriptFolder, unit ) )              //cleaning up after ourself
+						.catch( error => TESTER.log.error( error ) )                    //catch errors...
 				);
 			}
 
 			//finished with all tests
 			Promise.all( allTasks )
-			.catch( error => {
-				TESTER.log.error(`An error occurred: ${ Chalk.bgWhite.black(` ${ Path.basename( error ) } `) }`);
-
-				process.exit( 1 );
-			})
-			.then( () => {
-				if( TESTER.PASS ) {
-					TESTER.log.finished(`😅  All tests passed`);
-
-					process.exit( 0 );
-				}
-				else {
-					TESTER.log.finished(`😳  Ouch! Some tests failed`);
+				.catch( error => {
+					TESTER.log.error(`An error occurred: ${ Chalk.bgWhite.black(` ${ Path.basename( error ) } `) }`);
 
 					process.exit( 1 );
-				}
+				})
+				.then( () => {
+					if( TESTER.PASS ) {
+						TESTER.log.finished(`😅  All tests passed`);
+
+						process.exit( 0 );
+					}
+					else {
+						TESTER.log.finished(`😳  Ouch! Some tests failed`);
+
+						process.exit( 1 );
+					}
 			});
 
 		},
@@ -128,13 +148,17 @@ const TESTER = (() => { //constructor factory
 		 *
 		 * @param  {string} path     - The path to the folder that needs cleaning
 		 * @param  {object} settings - The settings object for this test
+		 *
+		 * @return {Promise object}
 		 */
 		delete: ( path, settings ) => {
 			const trash = [
-				Path.normalize(`${ path }/pancake`),
-				Path.normalize(`${ path }/testfolder`),
+				Path.normalize(`${ path }/pancake/`),
+				Path.normalize(`${ path }/testfolder/`),
 				Path.normalize(`${ path }/yarn.lock`),
 				Path.normalize(`${ path }/*.log.*`),
+				Path.normalize(`${ path }/fixture/.DS_Store`),
+				Path.normalize(`${ path }/fixture/*/.DS_Store`),
 			];
 
 			return new Promise( ( resolve, reject ) => {
@@ -143,7 +167,7 @@ const TESTER = (() => { //constructor factory
 						reject( error );
 					})
 					.then( paths => {
-						TESTER.log.pass(`Cleaned ${ Chalk.bgWhite.black(` ${ Path.basename( path ) } `) } folder`);
+						TESTER.log.pass(`Cleaned ${ Chalk.bgWhite.black(` ${ settings.folder } `) } folder`);
 
 						resolve();
 				});
@@ -156,10 +180,13 @@ const TESTER = (() => { //constructor factory
 		 *
 		 * @param  {string} path     - The path to the shell script
 		 * @param  {object} settings - The settings object for this test
+		 *
+		 * @return {Promise object}
 		 */
 		run: ( path, settings ) => {
 			return new Promise( ( resolve, reject ) => {
 
+				// what the command would look like:
 				// console.log('node ', [ Path.normalize(`${ path }/../../bin/pancake.js`), settings.script.command, path, ...settings.script.options ].join(' '));
 
 				Spawn
@@ -184,37 +211,111 @@ const TESTER = (() => { //constructor factory
 
 
 		/**
-		 * Compare the output of a test against its fixture
+		 * Get the checksum hash for the fixture of a test
 		 *
 		 * @param  {string} path     - The path to the test folder
 		 * @param  {object} settings - The settings object for this test
+		 *
+		 * @return {Promise object}  - The hash object of all files inside the fixture
 		 */
-		compare: ( path, settings ) => {
+		fixture: ( path, settings ) => {
 			return new Promise( ( resolve, reject ) => {
-				Dirsum.digest( Path.normalize(`${ path }/${ settings.compare }/`), 'sha256', ( error, hashes ) => {
+				Dirsum.digest( Path.normalize(`${ path }/fixture/${ settings.compare }/`), 'sha256', ( error, hashes ) => {
 					if( error ) {
-						console.error( error );
+						TESTER.log.pass( error );
 
 						TESTER.PASS = false;
 
 						reject( error );
 					}
 					else {
-
-						if( hashes.hash === settings.hash ) {
-							TESTER.log.pass(`${ Chalk.bgWhite.black(` ${ settings.name } `) } passed`);
-						}
-						else {
-							TESTER.log.fail(`${ Chalk.bgWhite.black(` ${ settings.name } `) } failed`);
-							TESTER.log.fail(`${ hashes.hash } !== ${ settings.hash }`);
-
-							TESTER.PASS = false;
-						}
-
-						resolve();
+						resolve( hashes );
 					}
 				});
 			});
+		},
+
+
+		/**
+		 * Get the checksum hash for the result of the test
+		 *
+		 * @param  {string} path     - The path to the test folder
+		 * @param  {object} settings - The settings object for this test
+		 * @param  {object} fixture  - The hash results of the fixture to be passed on
+		 *
+		 * @return {Promise object}  - The hash object of all files inside the resulting files
+		 */
+		result: ( path, settings, fixture ) => {
+			return new Promise( ( resolve, reject ) => {
+				Dirsum.digest( Path.normalize(`${ path }/${ settings.compare }/`), 'sha256', ( error, hashes ) => {
+					if( error ) {
+						TESTER.log.pass( error );
+
+						TESTER.PASS = false;
+
+						reject();
+					}
+					else {
+
+						resolve({ //passing it to compare later
+							fixture,
+							result: hashes,
+						});
+
+					}
+				});
+			});
+		},
+
+
+		/**
+		 * Compare the output of a test against its fixture
+		 *
+		 * @param  {object} settings - The settings object for this test
+		 * @param  {object} result   - The hash results of fixture and result
+		 *
+		 * @return {Promise object}  - The hash object of all files inside the fixture
+		 */
+		compare: ( settings, hashes ) => {
+
+			if( hashes.fixture.hash === hashes.result.hash ) {
+				TESTER.log.pass(`${ Chalk.bgWhite.black(` ${ settings.name } `) } passed`); //yay
+			}
+			else { //grr
+				TESTER.PASS = false;
+				TESTER.log.fail(`${ Chalk.bgWhite.black(` ${ settings.name } `) } failed`);
+
+				//flatten hash object
+				const fixture = flatten( hashes.fixture.files );
+				const result = flatten( hashes.result.files );
+
+				//iterate over fixture
+				for( const file of Object.keys( fixture ) ) {
+					const compare = result[ file ]; //get the hash from our result folder
+					delete result[ file ];          //remove this one so we can keep track of the ones that were not inside the fixture folder
+
+					if( compare === undefined ) {  //we couldn’t find this file inside the resulting folder
+						TESTER.log.error(`Missing ${ Chalk.yellow( file ) } file inside result folder`);
+					}
+					else {
+						const fileName = file.split('/');
+
+						if( fixture[ file ] !== compare && fileName[ fileName.length - 1 ] !== 'hash' ) { //we don’t want to compare folders
+							TESTER.log.error(`Difference inside ${ Chalk.yellow( file ) } file`);
+						}
+					}
+				}
+
+				if( Object.keys( result ).length > 0 ) { //found files that have not been deleted yet
+					let files = [];
+
+					for( const file of Object.keys( result ) ) {
+						files.push( file ); //make ’em readable
+					}
+
+					TESTER.log.error(`Some new files not accounted for: ${ Chalk.yellow( files.join(', ') ) } inside the fixture folder`);
+				}
+			}
 		},
 
 
@@ -252,7 +353,7 @@ const TESTER = (() => { //constructor factory
 			},
 
 			error: ( text ) => {
-				console.error(`\n\n        ${ Chalk.red( text ) }\u001b[1F`);
+				console.error(`\n        ${ Chalk.red( text ) }\u001b[1F`);
 			},
 
 			pass: ( text ) => {
