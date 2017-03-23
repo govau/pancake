b24ea8ad32aa3b7c8af3c6714b49735f57221915
Simon Schwartz <simon.schwartz0@gmail.com>
2017-03-24 09:43:36 +1100

    (#25) update unit tests for js plugin

--- packages/pancake-js/__tests__/js.spec.js
+++ packages/pancake-js/__tests__/js.spec.js
@@ -6,7 +6,7 @@
  *
  **************************************************************************************************************************************************************/
 
-import { MinifyJS } from '../src/js.js';
+import { MinifyJS, HandelJS, MinifyAllJS } from '../src/js.js';
 import Path from 'path';
 
 
@@ -28,4 +28,45 @@ console.error = jest.fn();
 
 test('MinifyJs should return same JS if it cannot minify the file', () => {
 	expect( MinifyJS( JsError, File ) ).toBe( JsError );
+});
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+// HandleJS function
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+const From = Path.normalize(`${ __dirname }/../../../tests/test2/node_modules/@gov.au/testmodule1/lib/js/module.js`);
+const Settings = {
+	"minified": true,
+	"modules": true,
+	"location": "pancake/js/",
+	"name": "pancake.min.js"
+}
+const To = Path.normalize(`${ __dirname }/../../../tests/test2/pancake/js/testmodule1.js`);
+const Tag = '@gov.au/testmodule1 v11.0.1'
+const Result = '/*! @gov.au/testmodule1 v11.0.1 */confirm(\"testmodule1:v11.0.1\");'
+
+test('HandelJS should return minified code from specified path', () => {
+	return HandelJS( From, Settings, To, Tag ).then( data => {
+		expect( data ).toBe( Result );
+	});
+});
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+// MinifyAllJS function
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+const Version = '1.0.8'
+const AllJS = []
+const SettingsAllJS = {
+	"minified": true,
+	"modules": true,
+	"location": "pancake/js/",
+	"name": "pancake.min.js"
+}
+const PkgPath = Path.normalize(`${ __dirname }/../../../tests/test2`);
+
+test('MinifyAllJS should resolve promise', () => {
+	return MinifyAllJS( Version, AllJS, SettingsAllJS, PkgPath ).then( data => {
+		expect( data ).toBe( true );
+	});
 });
